573e192ba212fecb902656cfd40186598b1ed255
joungeun <lje0818ovo@naver.com>
2022-04-25 13:18:08 +0900

    [ADD] #7 :: Song recommend create service

--- src/main/kotlin/com/dsm/rancuft/service/SongService.kt
+++ src/main/kotlin/com/dsm/rancuft/service/SongService.kt
@@ -1,11 +1,19 @@
 package com.dsm.rancuft.service
 
 import com.dsm.rancuft.dto.SongRequestDto
+import com.dsm.rancuft.entity.Song
+import com.dsm.rancuft.repository.SongRepository
 import org.springframework.stereotype.Service
 
 @Service
-class SongService {
+class SongService(private val songRepository: SongRepository) {
     fun createRecommendSong(dto:SongRequestDto) {
-
+        songRepository.save(
+            Song(
+                dto.singer,
+                dto.title,
+                dto.who
+            )
+        )
     }
 }
