4a89f8964c52ab05ee2a21dc0416fd1a430e672a
joungeun <lje0818ovo@naver.com>
2022-05-30 23:20:01 +0900

    [ADD] :: JwtFilter 및 token get claims, validate

---
+++ src/main/kotlin/com/dsm/rancuft/global/jwt/JwtFilter.kt
@@ -0,0 +1,37 @@
+package com.dsm.rancuft.global.jwt
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class JwtFilter(private val jwtUtil: JwtUtil) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token : String? = resolveToken(request)
+
+        if (token != null && jwtUtil.validateToken(token)) {
+            val authentication : Authentication = jwtUtil.getAuthentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun resolveToken(request: HttpServletRequest):String? {
+        val token :String = request.getHeader("Authorization")
+
+        if(token.startsWith("Bearer")) {
+            return token.substring(7)
+        }
+        return null
+    }
+
+}
--- src/main/kotlin/com/dsm/rancuft/global/jwt/JwtUtil.kt
+++ src/main/kotlin/com/dsm/rancuft/global/jwt/JwtUtil.kt
@@ -3,16 +3,27 @@ package com.dsm.rancuft.global.jwt
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.dsm.rancuft.dto.TokenResponseDto
+import com.dsm.rancuft.entity.User
+import com.dsm.rancuft.repository.UserRepository
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
+import java.lang.IllegalArgumentException
+import java.nio.charset.StandardCharsets
 import java.util.*
 
 @Component
-class JwtUtil {
+class JwtUtil (private val userRepository: UserRepository){
+
     @Value("\${auth.secret}")
     lateinit var secretKey:String
 
-    fun createToken(user: String, id: Int?):TokenResponseDto{
+    fun createToken(user: String, id: Int):TokenResponseDto{
         return TokenResponseDto(
             JWT.create()
                 .withClaim("name",user)
@@ -21,5 +32,35 @@ class JwtUtil {
                 .withExpiresAt(Date(System.currentTimeMillis()+30*10000*6000))
                 .sign(Algorithm.HMAC256(secretKey))
         )
+    }
+
+    fun validateToken(token: String) : Boolean {
+        try {
+            getClaims(token)
+            return true
+        } catch (e: Exception) {
+            throw IllegalArgumentException("token error")
+        }
+    }
+
+    fun getAuthentication(token: String) : Authentication{
+        return UsernamePasswordAuthenticationToken(getUser(token), null, null)
+    }
+
+    fun getUser(token: String): User {
+        return userRepository.findByIdOrNull(getUserId(token))
+            ?: throw IllegalArgumentException("not found user")
+    }
+
+    fun getUserId(token: String) : Int {
+        return getClaims(token)["id"] as Int
+    }
+
+    fun getClaims(token: String) : Claims {
+        val key = Keys.hmacShaKeyFor(secretKey.toByteArray(StandardCharsets.UTF_8))
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token).body
     }
 }
