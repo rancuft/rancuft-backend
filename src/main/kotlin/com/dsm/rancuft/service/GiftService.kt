4453bb15322517cd06917bff0c1d1a1e701f85ac
joungeun <lje0818ovo@naver.com>
2022-06-01 22:52:20 +0900

    [ADD] #8 :: comment 비어있을 시 랜덤 comment

--- src/main/kotlin/com/dsm/rancuft/service/GiftService.kt
+++ src/main/kotlin/com/dsm/rancuft/service/GiftService.kt
@@ -1,9 +1,11 @@
 package com.dsm.rancuft.service
 
 import com.dsm.rancuft.dto.GiftResponseDto
+import com.dsm.rancuft.entity.Comment
 import com.dsm.rancuft.entity.Gift
 import com.dsm.rancuft.entity.User
 import com.dsm.rancuft.global.exception.rs.GiftNotFoundException
+import com.dsm.rancuft.repository.CommentRepository
 import com.dsm.rancuft.repository.GiftRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -11,13 +13,17 @@ import org.springframework.web.multipart.MultipartFile
 import kotlin.random.Random
 
 @Service
-class GiftService(private val giftRepository: GiftRepository, private val s3ImageService: S3ImageService) {
-    fun giftSave(comment:String, gift:MultipartFile, user: User) {
+class GiftService(private val giftRepository: GiftRepository,
+                  private val s3ImageService: S3ImageService,
+                private val commentRepository: CommentRepository){
+    fun giftSave(comment:String, gift:MultipartFile, user: User) :String{
         val image = s3ImageService.uploadFile(gift)
 
         giftRepository.save(
-            Gift(image, comment,user)
+            Gift(image, comment, user)
         )
+
+        return image.url
     }
 
     fun giftGet() : GiftResponseDto{
@@ -25,7 +31,15 @@ class GiftService(private val giftRepository: GiftRepository, private val s3Imag
             randomNum(giftRepository.count()))
             ?: throw GiftNotFoundException()
 
-        return GiftResponseDto(gift.id!!,gift.image.url,gift.comment)
+        var comment: String = gift.comment
+
+        if (comment.isEmpty()) {
+            comment = commentRepository.findByIdOrNull(
+                randomNum(commentRepository.count())
+            )?.comment ?: "💛⭐"
+        }
+
+        return GiftResponseDto(gift.id!!,gift.image.url, comment)
     }
 
     private fun randomNum(max: Long) : Int {
