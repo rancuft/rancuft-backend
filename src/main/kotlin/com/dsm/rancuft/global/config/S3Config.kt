09acc8890e6b28bc2cd08617e9ecf4d1f586e1ef
joungeun <lje0818ovo@naver.com>
2022-06-02 12:02:46 +0900

    [MOVE] :: Config 폴더 생성

--- src/main/kotlin/com/dsm/rancuft/global/config/S3Config.kt
+++ src/main/kotlin/com/dsm/rancuft/global/config/S3Config.kt
@@ -1,4 +1,4 @@
-package com.dsm.rancuft.global.s3
+package com.dsm.rancuft.global.config
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
--- src/main/kotlin/com/dsm/rancuft/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/dsm/rancuft/global/config/SecurityConfig.kt
@@ -1,20 +1,29 @@
-package com.dsm.rancuft.global.security
+package com.dsm.rancuft.global.config
 
+import com.dsm.rancuft.global.jwt.JwtFilter
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @EnableWebSecurity
-class SecurityConfig: WebSecurityConfigurerAdapter(){
+class SecurityConfig(private val jwtFilter: JwtFilter): WebSecurityConfigurerAdapter(){
     override fun configure(http: HttpSecurity) {
         http
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .formLogin().disable()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .authorizeRequests()
+            .antMatchers("/user").permitAll()
+            .antMatchers(HttpMethod.GET, "/gift").permitAll()
+            .anyRequest().authenticated()
     }
 
     @Bean
